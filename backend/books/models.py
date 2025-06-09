f572e97d3f756895b7f9534cf037fd292f31ec03
dheerajdataclaps <dheerajdataclaps@gmail.com>
2025-06-09 21:10:01 +0530

    defined simple book app.

---
+++ backend/books/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=20)
+    summary = models.TextField()
