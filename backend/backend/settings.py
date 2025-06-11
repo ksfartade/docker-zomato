48e5df8bd42ed98316c206a7ba6532344dd8aa16
dheerajdataclaps <dheerajdataclaps@gmail.com>
2025-06-11 23:06:55 +0530

    updated static file setting.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,7 +128,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# For production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# For development
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
