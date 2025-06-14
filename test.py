33a36c181ae93f1c01c577250212b273783dbe5b
ksfartade <dheerajdataclaps@gmail.com>
2025-06-14 17:08:57 +0530

    Revert "added small error in test python file to check email notification."
    This reverts commit ce24274ae7922efa8e83c5cba3dea4d523a08d1e.
    
    removing bug of asd variable

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ def main():
     print("Starting the calculation...")
     result = cal(5, 3)
     print(f"The result is: {result}")
-asd 
+
 if __name__ == "__main__":
     main()
     print("Calculation completed.")
